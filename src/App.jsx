d6fd7389bfe8f46bb39ce13cd389a7740e2556d3
BrandonSGC <brangmz20@gmail.com>
2024-03-28 19:19:42 -0600

    Styling adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,75 @@
 import { Heading, NavBar, SkillsList } from "./components/";
 import { AboutMe, Hero } from "./layout/";
 
-import { skills } from './data/skills';
+import { skills } from "./data/skills";
 
 function App() {
   return (
     <>
-      <header className="relative">
+      <header className="fixed bg-slate-100 dark:bg-slate-950 w-full">
         <div className="mycontainer">
           <NavBar />
         </div>
       </header>
 
-      <section className="py-20">
+      <section className="pb-20 pt-20 md:pt-44">
         <div className="mycontainer flex items-center">
           <Hero />
         </div>
       </section>
 
       <section className="bg-slate-100 dark:bg-slate-800">
-        <div className="mycontainer py-20">
-          <Heading title='About Me'/>
+        <div className="mycontainer py-10">
+          <Heading title="About Me" />
           <AboutMe />
         </div>
       </section>
 
-      <section className="py-20">
+      <section className="py-10">
         <div className="mycontainer">
-          <Heading title="Skills" subtitle="The skills, tools and technologies that I work with"/>
+          <Heading
+            title="Skills"
+            subtitle="The skills, tools and technologies that I work with"
+          />
           <SkillsList skills={skills} />
         </div>
       </section>
 
+      <section className="py-10 bg-slate-100 dark:bg-slate-900">
+        <div className="mycontainer">
+          <Heading
+            title="Experience"
+            subtitle="The skills, tools and technologies that I work with"
+          />
+        </div>
+      </section>
+
+      <main className="py-10">
+        <div className="mycontainer">
+          <Heading
+            title="Projects"
+            subtitle="Some of the noteworthy projects I have built:"
+          />
+        </div>
+      </main>
+
+      <section className="py-10 bg-slate-100 dark:bg-slate-800">
+        <div className="mycontainer">
+          <Heading
+            title="Testimonials"
+            subtitle="Nice things people have said about me:"
+          />
+        </div>
+      </section>
+
+      <footer className="py-10">
+        <div className="mycontainer">
+          <Heading
+            title="Get in touch"
+            subtitle="What's next? Feel free to reach out to me if you're looking for a developer, have a query, or simply want to connect."
+          />
+        </div>
+      </footer>
     </>
   );
 }
