c9bffb54850b9e4661e878e78af040585a1abf79
BrandonSGC <brangmz20@gmail.com>
2024-04-02 11:43:27 -0600

    Skills hover effect

--- src/components/SkillsList.jsx
+++ src/components/SkillsList.jsx
@@ -1,8 +1,8 @@
 export const SkillsList = ({skills}) => {
   return (
-    <div className="flex flex-wrap gap-4 justify-center mt-10">
+    <div className="flex flex-wrap justify-center gap-4 mt-10">
       {skills.map(skill => (
-        <div key={skill.id}>
+        <div className="duration-300 hover:scale-105" key={skill.id}>
           <img className="mx-auto size-20" src={skill.iconURL} alt={`${skill.name} icon`} />
           <p className="text-center">{skill.name}</p>
         </div>
